c8555ebb9f8e375ada66d47eeaf53123b2b28096
Bitlatte <coby@bitlatte.dev>
2023-12-01 20:10:36 -0600

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Cup Noodle on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
